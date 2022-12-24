54379593065709a549a3f235d4e9b2fd174ab691
finlaywashere <finman292004@protonmail.com>
2022-12-23 21:55:25 -0500

    Deleted the planning bot stuff as it has been merged into the frosh bit

--- planning-bot/rebuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export NAME=planning-bot
-docker stop $NAME
-docker rm $NAME
-docker build -t engfrosh/$NAME --network host . --no-cache
-docker run -it -d --name $NAME --net host --restart unless-stopped engfrosh/$NAME
--- planning-bot/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/planning-bot/
-sudo -u ubuntu -g ubuntu ./run.sh

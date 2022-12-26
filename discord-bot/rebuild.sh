4b9690e27607299bcce8e6c305c33968a548fc3f
finlaywashere <finman292004@protonmail.com>
2022-12-26 12:57:33 -0500

    Added mount point to link the media dir for the bot and site to allow for verification photo upload through the discord bot

--- discord-bot/rebuild.sh
+++ discord-bot/rebuild.sh
@@ -3,4 +3,9 @@ export NAME=discord-bot
 docker stop $NAME
 docker rm $NAME
 docker build -t engfrosh/$NAME --network host . --no-cache
-docker run -it -d --name $NAME --net host --restart unless-stopped engfrosh/$NAME
+
+MEDIA_DIR=$(pwd)/../media/
+
+rm -rf $MEDIA_DIR
+
+docker run -it -d --name $NAME -v $MEDIA_DIR:/usr/share/engfrosh_site/files/media/ --net host --restart unless-stopped engfrosh/$NAME
--- engfrosh-site/rebuild.sh
+++ engfrosh-site/rebuild.sh
@@ -3,4 +3,9 @@ export NAME=engfrosh
 docker stop $NAME
 docker rm $NAME
 docker build -t engfrosh/$NAME --network host . --no-cache
-docker run -it -d --name $NAME -v $(pwd)/certs/:/etc/letsencrypt/ --net host --restart unless-stopped engfrosh/$NAME
+
+MEDIA_DIR=$(pwd)/../media/
+
+rm -rf $MEDIA_DIR
+
+docker run -it -d --name $NAME -v $(pwd)/certs/:/etc/letsencrypt/ -v $MEDIA_DIR:/usr/share/engfrosh_site/files/media/ --net host --restart unless-stopped engfrosh/$NAME

4f33174e6f53a6badbdb21daa83430ab2af0e922
finlaywashere <finman292004@protonmail.com>
2023-01-19 21:03:48 -0500

    added more cursed permissions

--- postgresql/setup.sh
+++ postgresql/setup.sh
@@ -8,6 +8,11 @@ docker exec postgres psql -U postgres -c "CREATE USER engfrosh_bot WITH PASSWORD
 docker exec postgres psql -U postgres -c "CREATE DATABASE engfrosh_dev_2022_07_05"
 docker exec postgres psql -U postgres -d engfrosh_dev_2022_07_05 -c "GRANT ALL ON SCHEMA public TO engfrosh_site"
 docker exec postgres psql -U postgres -d engfrosh_dev_2022_07_05 -c "GRANT ALL ON SCHEMA public TO engfrosh_bot"
+docker exec postgres psql -U postgres -d engfrosh_dev_2022_07_05 -c "GRANT pg_read_all_data TO engfrosh_bot;"
+docker exec postgres psql -U postgres -d engfrosh_dev_2022_07_05 -c "GRANT pg_write_all_data TO engfrosh_bot;"
+docker exec postgres psql -U postgres -d engfrosh_dev_2022_07_05 -c "GRANT pg_read_all_data TO engfrosh_site;"
+docker exec postgres psql -U postgres -d engfrosh_dev_2022_07_05 -c "GRANT pg_write_all_data TO engfrosh_site;"
+
 docker exec postgres psql -U postgres -d engfrosh_dev_2022_07_05 -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO engfrosh_site"
 docker exec postgres psql -U postgres -d engfrosh_dev_2022_07_05 -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO engfrosh_bot"
 docker exec postgres psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE engfrosh_dev_2022_07_05 TO engfrosh_site"

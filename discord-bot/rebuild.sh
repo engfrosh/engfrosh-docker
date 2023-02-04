c13584e932d973ce66339fd0a459d93ad46c812d
finlaywashere <finman292004@protonmail.com>
2023-02-04 13:58:59 -0500

    Added server credentials to discord bot env variables and removed media dir mount

--- discord-bot/rebuild.sh
+++ discord-bot/rebuild.sh
@@ -5,6 +5,4 @@ docker build -t engfrosh/$NAME --network host . --no-cache
 docker stop $NAME
 docker rm $NAME
 
-MEDIA_DIR=$(pwd)/../media/
-
-docker run -it -d --name $NAME -v $MEDIA_DIR:/usr/share/engfrosh_site/files/media/ --net host --restart unless-stopped engfrosh/$NAME
+docker run -it -d --name $NAME --net host --restart unless-stopped engfrosh/$NAME

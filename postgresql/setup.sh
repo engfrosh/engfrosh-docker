0d482330011f6582e3ac2d321977eb0625b3fda4
finlaywashere <finman292004@protonmail.com>
2023-02-10 00:59:00 -0500

    Added more perms for users

--- postgresql/setup.sh
+++ postgresql/setup.sh
@@ -17,3 +17,5 @@ docker exec postgres psql -U postgres -d engfrosh_dev_2022_07_05 -c "ALTER DEFAU
 docker exec postgres psql -U postgres -d engfrosh_dev_2022_07_05 -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO engfrosh_bot"
 docker exec postgres psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE engfrosh_dev_2022_07_05 TO engfrosh_site"
 docker exec postgres psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE engfrosh_dev_2022_07_05 TO engfrosh_bot"
+docker exec postgres psql -U postgres -d engfrosh_dev_2022_07_05 -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO engfrosh_site"
+docker exec postgres psql -U postgres -d engfrosh_dev_2022_07_05 -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO engfrosh_bot"

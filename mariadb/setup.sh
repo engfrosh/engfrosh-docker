98f7608a0d7fad8e045f68e4bcef01e46a2eff26
finlaywashere <finman292004@protonmail.com>
2023-02-10 13:02:28 -0500

    Changed db character set and fixed discord bot

--- mariadb/setup.sh
+++ mariadb/setup.sh
@@ -7,4 +7,4 @@ read -sp 'Root password: ' root_pass
 
 docker exec -it $NAME /usr/bin/mariadb-secure-installation
 docker exec $NAME bash -c "/usr/bin/mysql_tzinfo_to_sql /usr/share/zoneinfo | /usr/bin/mysql -u root --password=$root_pass mysql"
-docker exec $NAME mysql --password=$root_pass -e "CREATE DATABASE IF NOT EXISTS engfrosh CHARACTER SET utf8; USE engfrosh; SOURCE $SQLFILE;"
+docker exec $NAME mysql --password=$root_pass -e "CREATE DATABASE IF NOT EXISTS engfrosh CHARACTER SET utf8mb4; USE engfrosh; SOURCE $SQLFILE;"

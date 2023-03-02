253884c275fb00d551fc7c024ec2704aa7b65a5a
finlaywashere <finman292004@protonmail.com>
2023-03-02 12:13:03 -0500

    Made rebuild script automatically sets correct permissions for the media dir

--- engfrosh-site/rebuild.sh
+++ engfrosh-site/rebuild.sh
@@ -8,3 +8,5 @@ docker rm $NAME
 MEDIA_DIR=$(pwd)/../media/
 
 docker run -it -d --name $NAME -v $(pwd)/certs/:/etc/letsencrypt/ -v $MEDIA_DIR:/usr/share/engfrosh_site/files/media/ --net host --restart unless-stopped engfrosh/$NAME
+
+chown -R $(whoami):$(whoami) $MEDIA_DIR

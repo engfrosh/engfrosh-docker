3d1c56e48e9c1d180a46ec47a29810f5440d432b
Finlay Maroney <finman292004@protonmail.com>
2023-08-26 22:34:01 -0400

    Added restart command to small update

--- engfrosh-site/small_update.sh
+++ engfrosh-site/small_update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This script is for pulling quick hotfixes via git that don't require dep changes or model updates
 docker exec -it engfrosh bash -c 'git config --global --add safe.directory /home/ubuntu/engfrosh && cd /home/ubuntu/engfrosh && git pull'
+docker restart engfrosh

c43dd9b591b86b557738e3102242d875bcad9be5
finlaywashere <finman292004@protonmail.com>
2023-08-31 19:46:36 -0400

    fixed naming issue

--- backup_media.sh
+++ backup_media.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker system prune -f
-docker run --rm --volumes-from engfrosh -v /home/admin/backup:/backup-dir ubuntu tar czvf "/backup-dir/media-$(date '+%F').tar.gz" /usr/share/engfrosh_site/files/media/
+docker run --rm --volumes-from engfrosh -v /home/admin/backup:/backup-dir ubuntu tar czvf "/backup-dir/media-$(date '+%F-%T').tar.gz" /usr/share/engfrosh_site/files/media/

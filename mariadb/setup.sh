3d3cf23c15c2ba5c186b0c74d3ee5f2612a1a1fd
Finlay Maroney <finman292004@protonmail.com>
2024-01-02 12:42:43 -0500

    fixed mariadb setup.sh

--- mariadb/setup.sh
+++ mariadb/setup.sh
@@ -6,5 +6,5 @@ SQLFILE='/setup.sql'
 read -sp 'Root password: ' root_pass
 
 docker exec -it $NAME /usr/bin/mariadb-secure-installation
-docker exec $NAME bash -c "/usr/bin/mysql_tzinfo_to_sql /usr/share/zoneinfo | /usr/bin/mysql -u root --password=$root_pass mysql"
-docker exec $NAME mysql --password=$root_pass -e "CREATE DATABASE IF NOT EXISTS engfrosh CHARACTER SET utf8mb4; USE engfrosh; SOURCE $SQLFILE;"
+docker exec $NAME bash -c "/usr/bin/mariadb-tzinfo-to-sql /usr/share/zoneinfo | /usr/bin/mariadb -u root --password=$root_pass mysql"
+docker exec $NAME mariadb --password=$root_pass -e "CREATE DATABASE IF NOT EXISTS engfrosh CHARACTER SET utf8mb4; USE engfrosh; SOURCE $SQLFILE;"

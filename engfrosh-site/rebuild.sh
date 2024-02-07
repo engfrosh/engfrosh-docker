67fb9b93f237ac2474cc6f50c81560289721e5be
Finlay Maroney <finman292004@protonmail.com>
2024-02-06 19:53:32 -0500

    Fixed nginx conf and switched networks

--- engfrosh-site/rebuild.sh
+++ engfrosh-site/rebuild.sh
@@ -7,6 +7,6 @@ docker rm $NAME
 
 MEDIA_DIR=$(pwd)/../media/
 
-docker run -it -d --name $NAME -v $(pwd)/certs/:/etc/letsencrypt/ -v $MEDIA_DIR:/usr/share/engfrosh_site/files/media/ --net host --restart unless-stopped engfrosh/$NAME
+docker run -it -d --name $NAME -v $(pwd)/certs/:/etc/letsencrypt/ -v $MEDIA_DIR:/usr/share/engfrosh_site/files/media/ --net httpd-net --restart unless-stopped -e VIRTUAL_HOST=nem-scav.mycses.ca engfrosh/$NAME
 
 chown -R $(whoami):$(whoami) $MEDIA_DIR

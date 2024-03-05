02336f888031d09099099987c352b8c05eaed35b
Finlay Maroney <finman292004@protonmail.com>
2024-03-04 19:19:04 -0500

    fixed perms

--- engfrosh-site/rebuild.sh
+++ engfrosh-site/rebuild.sh
@@ -9,4 +9,4 @@ MEDIA_DIR=$(pwd)/../media/
 
 docker run -it -d --name $NAME -v $(pwd)/certs/:/etc/letsencrypt/ -v $MEDIA_DIR:/usr/share/engfrosh_site/files/media/ --net httpd-net --restart unless-stopped -e VIRTUAL_HOST=nem-scav.mycses.ca cses/$NAME
 
-chown -R $(whoami):$(whoami) $MEDIA_DIR
+chown -R 1000:1000 $MEDIA_DIR

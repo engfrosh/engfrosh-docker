0cede6e384bb96a95968e64b8f07415c575bfa15
Finlay Maroney <finman292004@protonmail.com>
2024-03-23 19:42:49 -0400

    Added gitignore and better backup scripts

--- backup_db.sh
+++ backup_db.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-docker system prune -f
 docker exec mariadb mariadb-dump --all-databases > /var/backups/$(date +"%Y-%m-%d-%T")_backup.sql
 mv mariadb/backups/* /var/backups/
--- backup_media.sh
+++ backup_media.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker system prune -f
-docker run --rm --volumes-from engfrosh -v /home/admin/backup:/backup-dir ubuntu tar czvf "/backup-dir/media-$(date '+%F-%T').tar.gz" /usr/share/engfrosh_site/files/media/
+tar czvf "/var/backups/$(date '+%F-%T')-media.tar.gz" media/

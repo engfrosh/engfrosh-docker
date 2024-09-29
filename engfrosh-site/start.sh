3d4d2069c69741176b7573b887a89cf8e9ed33c8
Finlay Maroney <finlay@finlaym.xyz>
2024-09-29 18:52:57 -0400

    switched to uvicorn

--- engfrosh-site/start.sh
+++ engfrosh-site/start.sh
@@ -4,4 +4,4 @@ source ./environment
 cd engfrosh/engfrosh_site/
 nginx
 rm -f /home/ubuntu/engfrosh/engfrosh_site/engfrosh_site.sock /home/ubuntu/engfrosh/engfrosh_site/engfrosh_site.sock.lock
-sudo -E -u ubuntu -g www-data daphne -u /home/ubuntu/engfrosh/engfrosh_site/engfrosh_site.sock engfrosh_site.asgi:application
+sudo -E -u ubuntu -g www-data uvicorn --uds /home/ubuntu/engfrosh/engfrosh_site/engfrosh_site.sock engfrosh_site.asgi:application

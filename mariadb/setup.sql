850883db67c51b92611247712012f823edc77b48
finlaywashere <finman292004@protonmail.com>
2025-01-30 16:56:33 -0500

    Made tweaks to help with testing and removed TZ variable

--- mariadb/setup.sql
+++ mariadb/setup.sql
@@ -4,5 +4,5 @@ SET PASSWORD FOR engfrosh_site = PASSWORD('there-exercise-fenegle');
 CREATE USER IF NOT EXISTS engfrosh_bot;
 SET PASSWORD FOR engfrosh_bot = PASSWORD('mypassword');
 
-GRANT ALL PRIVILEGES ON engfrosh.* TO engfrosh_site;
-GRANT ALL PRIVILEGES ON engfrosh.* TO engfrosh_bot;
+GRANT ALL PRIVILEGES ON *.* TO engfrosh_site;
+GRANT ALL PRIVILEGES ON *.* TO engfrosh_bot;

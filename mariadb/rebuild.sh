6b8c3569d633dbc7f1bb30cca89b18403d06f389
Finlay Maroney <finlay@finlaym.xyz>
2025-02-16 12:59:25 -0500

    removed makemigrations from dockerfile

--- mariadb/rebuild.sh
+++ mariadb/rebuild.sh
@@ -5,4 +5,4 @@ docker build -t engfrosh/$NAME . --no-cache
 docker stop $NAME
 docker rm $NAME
 
-docker run -it -d --name $NAME --net host -v $(pwd)/backups:/var/backups --mount source=db,target=/var/lib/mysql/ -e MARIADB_ROOT_PASSWORD="" --restart unless-stopped engfrosh/$NAME '--wait_timeout=31536000 --max_connections=100'
+docker run -it -d --name $NAME --net host -v $(pwd)/backups:/var/backups --mount source=db,target=/var/lib/mysql/ -e MARIADB_ROOT_PASSWORD="mariadbpass" --restart unless-stopped engfrosh/$NAME --wait_timeout=31536000 --max_connections=100

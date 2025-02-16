d983df0d36bfae0ec01a10245273ba96b0fde463
Finlay Maroney <finlay@finlaym.xyz>
2025-02-16 13:00:44 -0500

    Added more packages to dockerfile

--- engfrosh-site/rebuild.sh
+++ engfrosh-site/rebuild.sh
@@ -5,7 +5,7 @@ docker build -t engfrosh/$NAME --network host . --no-cache
 docker stop $NAME
 docker rm $NAME
 
-MEDIA_DIR=$(pwd)/../media/
+MEDIA_DIR=/var/media/
 
 docker run -it -d --name $NAME -v $(pwd)/certs/:/etc/letsencrypt/ -v $MEDIA_DIR:/usr/share/engfrosh_site/files/media/ -e WORKERS=50 --net host --restart unless-stopped --log-driver=journald engfrosh/$NAME
 

005dccf64382c092d0d4d9ab889912a4e420047e
Finlay Maroney <finlay@finlaym.xyz>
2025-02-17 13:59:00 -0500

    Fixed bug in mariadb script

--- mariadb/rebuild.sh
+++ mariadb/rebuild.sh
@@ -5,4 +5,4 @@ docker build -t engfrosh/$NAME . --no-cache
 docker stop $NAME
 docker rm $NAME
 
-docker run -it -d --name $NAME --net host -v $(pwd)/backups:/var/backups --mount source=db,target=/var/lib/mysql/ -e MARIADB_ROOT_PASSWORD="test" --restart unless-stopped engfrosh/$NAME '--wait_timeout=31536000 --max_connections=100'
+docker run -it -d --name $NAME --net host -v $(pwd)/backups:/var/backups --mount source=db,target=/var/lib/mysql/ -e MARIADB_ROOT_PASSWORD="test" --restart unless-stopped engfrosh/$NAME --wait_timeout=31536000 --max_connections=100

865254f004006d2e1e7c9154c3bbcef61c06ef64
Saer <git@sredwards.com>
2026-03-25 20:47:08 -0400

    docs

--- engfrosh-site/small_update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This script is for pulling quick hotfixes via git that don't require dep changes or model updates
-docker exec -it engfrosh bash -c 'git config --global --add safe.directory /app/engfrosh && cd /app/engfrosh && git pull'
-docker restart engfrosh
